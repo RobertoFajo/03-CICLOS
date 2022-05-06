459176c53cbe862d655c00ee9bbe7d13762ed081
Roberto Fajo <you@example.com~robertofajo@gmail.com>
2022-05-06 07:03:37 -0300

    Completo con funciones

--- js/logica.js
+++ js/logica.js
@@ -46,43 +46,28 @@
 // }
     
 
-window.alert("Comienza ejercicio 5, estás listo?");
-let n;
-let contador=0;
-let validaNumero;
-for(let x=1;x<=5;x++){
-    n=pideValidaNumeroEntero("Ingrese edad: ");
-    if(n%2>0 && n>18){
-        contador++;
-    }
-}
-window.alert("Hubo "+contador+" edades impares mayores a 18");
+// window.alert("Comienza ejercicio 5, estás listo?");
+// let n;
+// let contador=0;
+// let validaNumero;
+// for(let x=1;x<=5;x++){
+//     n=pideValidaNumeroEntero("Ingrese edad: ");
+//     if(n%2>0 && n>18){
+//         contador++;
+//     }
+// }
+// window.alert("Hubo "+contador+" edades impares mayores a 18");
 
 
 // window.alert("Comienza ejercicio 6, estás listo?");
 // let n;
-// let validaNumero;
-// let monto;
+// let monto=0;
 // let ventasTotales=0;
-// n=prompt("Cuántas ventas vas a ingresar? ");
-// validaNumero=isNaN(n);
-// while(validaNumero==true){
-//     window.alert("El ingreso incorrecto");
-//     n=prompt("Cuántas ventas vas a ingresar? ");
-//     validaNumero=isNaN(n);
-// }
-// n=parseInt(n);
+// n=pideValidaNumeroEntero("Cuántas ventas vas a ingresar?: ");
 // for(x=1;x<=n;x++){
-//     monto=prompt("Ingresa monto de venta "+x);
-//     validaNumero=isNaN(monto);
-//     while(validaNumero==true){
-//         window.alert("El ingreso incorrecto");
-//         monto=prompt("Ingresa monto de venta "+x);
-//         validaNumero=isNaN(n);
-//     }
-//     monto=parseFloat(monto);
+//     monto=pideValidaNumero("Ingresa monto de venta "+x);
 //     ventasTotales+=monto;
-// }
+//     }
 // window.alert("El total de las ventas ingresadas es: "+ventasTotales);
 
 
@@ -91,23 +76,9 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let sumaAlturas=0;
 // let altura;
 // let validaNumero;
-// cantJugadores=prompt("Cuántas Jugadores vas a ingresar? ");
-// validaNumero=isNaN(cantJugadores);
-// while(validaNumero==true){
-//     window.alert("El ingreso incorrecto");
-//     cantJugadores=prompt("Cuántas Jugadores vas a ingresar? ");
-//     validaNumero=isNaN(cantJugadores);
-// }
-// cantJugadores=parseInt(cantJugadores);
-// console.log(cantJugadores);
+// cantJugadores=pideValidaNumeroEntero("Cuántas Jugadores vas a ingresar? ");
 // for(x=1;x<=cantJugadores;x++){
-//     altura=prompt("Ingresa Altura del Jugador "+x);
-//     validaNumero=isNaN(altura);
-//     while(validaNumero==true){
-//         window.alert("El ingreso incorrecto");
-//         altura=prompt("Ingresa Altura del Jugador "+x);
-//         validaNumero=isNaN(n);
-//     }
+//     altura=pideValidaNumero("Ingresa Altura del Jugador "+x);
 //     altura=parseFloat(altura);
 //     sumaAlturas+=altura;
 // }
@@ -121,42 +92,14 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let nroMayor;
 // let posNumeroMayor;
 // let numero;
-// cantNumeros=prompt("Cuantos números enteros vas a ingresar?");
-// validaNumero=isNaN(cantNumeros);
-// console.log(cantNumeros);
-// console.log(parseInt(cantNumeros));
-// console.log(parseFloat(cantNumeros));
-// while(validaNumero==true || !(parseInt(cantNumeros)==parseFloat(cantNumeros))){
-//     window.alert("El ingreso incorrecto");
-//     cantNumeros=prompt("Cuantos números enteros vas a ingresar?");
-//     validaNumero=isNaN(cantNumeros);
-// }
-// cantNumeros=parseInt(cantNumeros);
-// numero=prompt("1) ingresa un número entero: ");
-// validaNumero=isNaN(numero);
-// console.log(numero);
-// console.log(parseInt(numero));
-// console.log(parseFloat(numero));
-// while(validaNumero==true || !(parseInt(numero)==parseFloat(numero))){
-//     window.alert("El ingreso incorrecto");
-//     numero=prompt("Cuantos números enteros vas a ingresar?");
-//     validaNumero=isNaN(numero);
-// }
+// cantNumeros=pideValidaNumeroEntero("Cuantos números enteros vas a ingresar?");
+// numero=pideValidaNumeroEntero("1) ingresa un número entero: ");
 // nroMayor=parseInt(numero);
 // posNumeroMayor=1;
 // for(let x=2;x<=cantNumeros;x++){
-//     numero=prompt(x+") ingresa un número entero: ");
-//     validaNumero=isNaN(numero);
-//     console.log(numero);
-//     console.log(parseInt(numero));
-//     console.log(parseFloat(numero));
-//     while(validaNumero==true || !(parseInt(numero)==parseFloat(numero))){
-//         window.alert("El ingreso incorrecto");
-//         numero=prompt("Cuantos números enteros vas a ingresar?");
-//         validaNumero=isNaN(numero);
-//     }
-//     if(parseInt(numero)>nroMayor){
-//         nroMayor=parseInt(numero);
+//     numero=pideValidaNumeroEntero(x+") ingresa un número entero: ");
+//     if(numero>nroMayor){
+//         nroMayor=numero;
 //         posNumeroMayor=x;
 //     }
 // }
@@ -167,13 +110,7 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let numero;
 // let validaNumero;
 // let factorial=0;
-// numero=prompt("ingresa un número entero para calcular su factorial: ");
-// validaNumero=isNaN(numero);
-// while(validaNumero==true || !(parseInt(numero)==parseFloat(numero))){
-//     window.alert("El ingreso incorrecto");
-//     numero=prompt("ingresa un número entero para calcular su factorial: ");
-//     validaNumero=isNaN(numero);
-// }
+// numero=pideValidaNumeroEntero("ingresa un número entero para calcular su factorial: ");
 // factorial=1;
 // for(let x=2;x<=numero;x++){
 //     factorial*=x;
@@ -186,32 +123,14 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // window.alert("Comienza ejercicio 10, estás listo?");
 // let validaNumero;
 // let numero;
-// numero=prompt("Ingresa la nota: ");
-// validaNumero=isNaN(numero);
-// while(validaNumero==true || !(numero>=0 && numero<=10)){
-//     window.alert("El ingreso incorrecto");
-//     numero=prompt("Ingresa la nota: ");
-//     validaNumero=isNaN(numero);
-// }
-// numero=parseFloat(numero);
+// numero=pideValidaNumero("Ingresa la nota: ");
 // window.alert("La nota ingresada es "+numero);
 
 
 // window.alert("Comienza ejercicio 12, estás listo?");
 // let continuar;
 // window.alert("************* INICIA VALIDACION CON WHILE ****************");
-// continuar=prompt("Desea continuar? [S/N]");
-// while(continuar!="S" && continuar!="N" && continuar!="s" && continuar!="n"){
-//     window.alert("Ingreso incorrecto, intente nuevamente.");
-//     continuar=prompt("Desea continuar? [S/N]");
-// }
-// window.alert("************* INICIA VALIDACION CON DO WHILE ****************");
-// do{
-//     continuar=prompt("Desea continuar? [S/N]");
-//     if(continuar!="S" && continuar!="N" && continuar!="s" && continuar!="n"){
-//         window.alert("Ingreso incorrecto, intente nuevamente.");
-//     }
-// }while(continuar!="S" && continuar!="N" && continuar!="s" && continuar!="n");
+// continuar=validaSN("Desea continuar? [S/N]");
 // window.alert("************* FIN DE EJERCICIO ****************");
 
 
@@ -219,14 +138,7 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let numero;
 // let nroMayor;
 // let nroMenor; 
-// numero=prompt("Ingresa un número: ");
-// validaNumero=isNaN(numero);
-// while(validaNumero==true){
-//     window.alert("El ingreso incorrecto");
-//     numero=prompt("Ingresa un número: ");
-// validaNumero=isNaN(numero);
-// }
-// numero=parseFloat(numero);
+// numero=pideValidaNumero("Ingresa un número: ");
 // nroMayor=numero;
 // nroMenor=numero;
 // while(numero!=0){
@@ -237,15 +149,7 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 //             nroMayor=numero;
 //         }
 //     }
-//     numero=prompt("Ingresa un número: ");
-//     validaNumero=isNaN(numero);
-//     while(validaNumero==true){
-//         window.alert("El ingreso incorrecto");
-//         numero=prompt("Ingresa un número: ");
-//     validaNumero=isNaN(numero);
-//     }
-//     numero=parseFloat(numero);
-    
+//     numero=pideValidaNumero("Ingresa un número: ");
 // }
 // window.alert("El mayor es "+nroMayor+" y el menor "+nroMenor);
 
@@ -258,13 +162,7 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let edadMasJoven;
 // nombre=prompt("Ingresa nombre de la persona "+contador+":")
 // if(nombre!="*"){
-//     edad=prompt("ingresa la edad de la persona "+contador+":");
-//     validaNumero=isNaN(edad);
-//     while(validaNumero==true || !(parseInt(edad)==parseFloat(edad))){
-//         window.alert("El ingreso incorrecto");
-//         edad=prompt("ingresa la edad de la persona "+contador+":");
-//         validaNumero=isNaN(edad);
-//     }
+//     edad=pideValidaNumeroEntero("ingresa la edad de la persona "+contador+":");
 //     nombreMasJoven=nombre;
 //     edadMasJoven=edad;
 // }
@@ -272,13 +170,7 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 //     contador++;
 //     nombre=prompt("Ingresa nombre de la persona "+contador+":")
 //     if(nombre!="*"){
-//         edad=prompt("ingresa la edad de la persona "+contador+":");
-//         validaNumero=isNaN(edad);
-//         while(validaNumero==true || !(parseInt(edad)==parseFloat(edad))){
-//             window.alert("El ingreso incorrecto");
-//             edad=prompt("ingresa la edad de la persona "+contador+":");
-//             validaNumero=isNaN(edad);
-//         }
+//         edad=pideValidaNumeroEntero("ingresa la edad de la persona "+contador+":");
 //         if(edad<edadMasJoven){
 //             edadMasJoven=edad;
 //             nombreMasJoven=nombre;
@@ -297,26 +189,12 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let promedio;
 // let contador=0;
 // let sumaNumeros;
-// numero=prompt("Ingresa un número: ");
-// validaNumero=isNaN(numero);
-// while(validaNumero==true){
-//     window.alert("El ingreso incorrecto");
-//     numero=prompt("Ingresa un número: ");
-// validaNumero=isNaN(numero);
-// }
-// numero=parseFloat(numero);
+// numero=pideValidaNumero("Ingresa un número: ");
 // promedio=numero;
 // sumaNumeros=numero;
 // contador++;
 // while(promedio<20){
-//     numero=prompt("Ingresa un número: ");
-//     validaNumero=isNaN(numero);
-//     while(validaNumero==true){
-//         window.alert("El ingreso incorrecto");
-//         numero=prompt("Ingresa un número: ");
-//         validaNumero=isNaN(numero);
-//     }
-//     numero=parseFloat(numero);
+//     numero=pideValidaNumero("Ingresa un número: ");
 //     contador++;
 //     sumaNumeros+=numero;
 //     promedio=sumaNumeros/contador;
@@ -331,24 +209,12 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let porcentaje;
 // let respuesta="s"
 // while(respuesta=="s" || respuesta=="S"){
-//     numero=prompt("Ingresa un número entero:");
-//     validaNumero=isNaN(numero);
-//     while(validaNumero==true || !(parseInt(numero)==parseFloat(numero))){
-//         window.alert("El ingreso incorrecto");
-//         numero=prompt("Ingresa un número entero:");
-//         validaNumero=isNaN(numero);
-//     }
-//     numero=parseInt(numero);
-//     if(numero/2==parseInt(numero/2)){
+//     numero=pideValidaNumeroEntero("Ingresa un número entero:");
+//     if(numero%2==0){
 //         totalPares++;
 //     }
 //     totalNumeros++;
-//     numero=parseInt(numero);
-//     respuesta=prompt("Desea continuar? [S/N]");
-//     while(respuesta!="S" && respuesta!="N" && respuesta!="s" && respuesta!="n"){
-//         window.alert("Ingreso incorrecto, intente nuevamente.");
-//         respuesta=prompt("Desea continuar? [S/N]");
-//     }
+//     respuesta=validaSN("Desea continuar? [S/N]");
 // }
 // porcentaje=(totalPares/totalNumeros)*100;
 // window.alert("El "+porcentaje+" de los númeeros es par");
@@ -361,50 +227,25 @@ window.alert("Hubo "+contador+" edades impares mayores a 18");
 // let totalTicket=0;
 // let respuesta="s"
 // while(respuesta=="s" || respuesta=="S"){
-//     precioArticulo=prompt("Ingresa precio del artículo:");
-//     validaNumero=isNaN(precioArticulo);
-//     while(validaNumero==true){
-//         window.alert("El ingreso incorrecto");
-//         precioArticulo=prompt("Ingresa precio del artículo:");
-//         validaNumero=isNaN(precioArticulo);
-//     }
-//     precioArticulo=parseFloat(precioArticulo);
-//     cantidad=prompt("ingresa la cantidad:");
-//         validaNumero=isNaN(cantidad);
-//         while(validaNumero==true || !(parseInt(cantidad)==parseFloat(cantidad))){
-//             window.alert("El ingreso incorrecto");
-//             cantidad=prompt("ingresa la cantidad:");
-//             validaNumero=isNaN(cantidad);
-//         }
-//     cantidad=parseInt(cantidad);
+//     precioArticulo=pideValidaNumero("Ingresa precio del artículo:");
+//     cantidad=pideValidaNumeroEntero("ingresa la cantidad:");
 //     parcial=precioArticulo*cantidad;
 //     totalTicket+=parcial;
-//     respuesta=prompt("Desea continuar? [S/N]");
-//     while(respuesta!="S" && respuesta!="N" && respuesta!="s" && respuesta!="n"){
-//         window.alert("Ingreso incorrecto, intente nuevamente.");
-//         respuesta=prompt("Desea continuar? [S/N]");
-//     }
+//     respuesta=validaSN("Desea continuar? [S/N]");
 // }
 // window.alert("El total de la compra es: "+totalTicket+" pesos");
 
 
-// window.alert("Comienza ejercicio 18, estás listo?");
-// let sueldo=0;
-// let sueldoAnual=0;
-// contador=1;
-// while(sueldo>=0 && contador<=12){
-//     sueldo=prompt("Ingresa sueldo del mes "+contador);
-//     validaNumero=isNaN(sueldo);
-//     while(validaNumero==true){
-//         window.alert("El ingreso incorrecto");
-//         sueldo=prompt("Ingresa sueldo del mes "+x);
-//         sueldo=isNaN(sueldo);
-//     }
-//     sueldo=parseFloat(sueldo);
-//     sueldoAnual+=sueldo;
-//     contador++;
-// }
-// window.alert("El sueldo anual percibido es de $"+sueldoAnual)
+window.alert("Comienza ejercicio 18, estás listo?");
+let sueldo=0;
+let sueldoAnual=0;
+contador=1;
+while(sueldo>=0 && contador<=12){
+    sueldo=pideValidaNumero("Ingresa sueldo del mes "+contador);
+    sueldoAnual+=sueldo;
+    contador++;
+}
+window.alert("El sueldo anual percibido es de $"+sueldoAnual)
 
 
 // window.alert("Comienza ejercicio 19, estás listo?");
--- js/my-functions.js
+++ js/my-functions.js
@@ -33,3 +33,34 @@ function pideValidaNumeroEnteroEntre(mensaje,n1,n2){
     }
     return numero;
 }
+
+
+function pideValidaNumero(mensaje){
+    let numero;
+    let validaNumero;
+    let error;
+    let valido=0;
+    numero=prompt(mensaje);
+    while(valido==0){
+        validaNumero=isNaN(parseFloat(numero));
+        if(validaNumero==true){
+            error=("ERROR!!!! No es un número. "+mensaje);
+        }else{
+                valido=1;
+        }
+        if(valido==0){
+            numero=prompt(error);
+        }
+    }
+    return parseFloat(numero);
+}
+
+
+function validaSN(mensaje){
+    continuar=prompt(mensaje);
+    while(continuar!="S" && continuar!="N" && continuar!="s" && continuar!="n"){
+        window.alert("Ingreso incorrecto, intente nuevamente.");
+        continuar=prompt(mensaje);
+    }
+    return continuar;
+}
